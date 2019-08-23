13aa321c617df50191a55b683337160cd70f88fe
shubhindia123 <shubhindia123@gmail.com>
2019-08-23 09:11:06 +0000

    fix some things
    Signed-off-by: shubhindia123 <shubhindia123@gmail.com>

--- ip_logger.sh
+++ ip_logger.sh
@@ -4,5 +4,5 @@ echo "chmod 777 /var/log/history
 USER_IP=`who -u am i 2>/dev/null| awk '{print $NF}'|sed -e 's/[()]//g'`
 export HISTFILE=/var/log/history/$(whoami)-$USER_IP-history
 " >> /root/.bashrc
-
+source /root/.bashrc
 
